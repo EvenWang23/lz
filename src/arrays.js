7c718f20facd4d0c5125ef62cfc1d8ebf32c5ef4
wangyiwen <1766236040@qq.com>
2024-04-13 12:12:32 +0800

    add some function for array operator

--- src/arrays.js
+++ src/arrays.js
@@ -1,13 +1,64 @@
 /**
- *
- * @param {Array} array The array to obtain length
- * @return {number} Returns `array.length`
+ *Get the length of the array.
+ * @param {Array} array The array to obtain length.
+ * @return {number} Returns `array.length`.
  */
 function getLength(array) {
     return array == null ? 0 : array.length
 }
 
+/**
+ *
+ * @param {Array} array The array to iteratee over.
+ * @param {Function} iteratee The function invoked per iteration.
+ * @return {Array} Returns `array`
+ */
+function each(array, iteratee) {
+    let index = -1,
+        length = getLength(array)
+
+    while (++index < length) {
+        if (iteratee(array[index], index, array) === false) {
+            break
+        }
+    }
+    return array
+}
+
+/**
+ *
+ * @param {Array} array The array to slice.
+ * @param {Number} [start=0] The start position(include).
+ * @param {Number} [end=array.length] The end position(exclude).
+ * @return {Array} Returns the slice of `array`
+ */
+function slice(array, start, end) {
+    let length = getLength(array),
+        index = -1
+
+    if (length === 0) {
+        return Array(0)
+    }
+
+    if (start < 0) {
+        start = -start > length ? 0 : length + start
+    }
+    end = end < length ? end : length
+    if (end < 0) {
+        end = length + end
+    }
+    start >>>= 0
+    length = end > start ? (end - start >>> 0) : 0
+
+    const result = Array(length)
+    while (++index < length) {
+        result[index] = array[index + start]
+    }
+    return result
+}
 
 export default {
-    getLength
+    getLength,
+    each,
+    slice
 }
--- test/arrays.spec.js
+++ test/arrays.spec.js
@@ -7,4 +7,15 @@ describe('arrays', () => {
         expect(arrays.getLength(void 0)).be(0)
         expect(arrays.getLength([1, 2, 3])).be(3)
     })
+
+    it('each', () => {
+        let result = 0
+        arrays.each([1, 2, 3, 4], num => result += num)
+        expect(result).be(10)
+    })
+
+    it('slice', () => {
+        expect(arrays.slice([1, 2, 3, 4, 5, 6], 0, 3)).eql([1, 2, 3])
+        expect(arrays.slice([1, 2, 3, 4, 5, 6], -1, 6)).eql([6])
+    })
 })
