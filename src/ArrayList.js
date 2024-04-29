eaa87ba8a7f8e95aabe67d28db4c5ae1749be664
wangyiwen <1766236040@qq.com>
2024-04-29 22:38:31 +0800

    add remove for ArrayList

--- src/ArrayList.js
+++ src/ArrayList.js
@@ -3,24 +3,37 @@ import arrays from './arrays.js'
 class ArrayList {
     constructor() {
         this.__elementData__ = []
+        this.__size__ = 0
     }
 
     add(element) {
         this.__elementData__.push(element)
+        this.__size__++
     }
 
     size() {
-        return this.__elementData__.length
+        return this.__size__
     }
 
     insert(element, index) {
         const elementData = this.__elementData__
         arrays.arraycopy(elementData, index, elementData, index + 1, this.size() - index)
+        this.__size__++
         elementData[index] = element
     }
 
     get(index) {
         return this.__elementData__[index]
+    }
+
+    remove(index) {
+        const elementData = this.__elementData__
+        const s = this.size() - 1
+        const oldValue = elementData[index]
+        delete elementData[index]
+        arrays.arraycopy(elementData, index + 1, elementData, index, s - index)
+        this.__size__--
+        return oldValue
     }
 }
 
--- test/ArrayList.spec.js
+++ test/ArrayList.spec.js
@@ -23,4 +23,17 @@ describe('ArrayList.', () => {
         expect(list.get(1)).be(1)
         expect(list.size()).be(2)
     })
+
+
+    it('remove', () => {
+        const list = new ArrayList()
+        list.add(1)
+        list.add(2)
+        list.add(3)
+        expect(list.size()).be(3)
+        expect(list.get(1)).be(2)
+        list.remove(1)
+        expect(list.get(1)).be(3)
+        expect(list.size()).be(2)
+    })
 })
